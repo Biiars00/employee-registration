c0d3ad00050bc1c2670ee46047a4471fe9e908fb
Beatriz Ribeiro <biiaribeiro60@gmail.com>
2025-02-10 17:20:52 -0300

    refactor: dotenv path configuration.

--- src/main/java/registration_system/Main.java
+++ src/main/java/registration_system/Main.java
@@ -8,20 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure().directory("./").load();
 
 		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
 		System.setProperty("DATABASE_USER", dotenv.get("DATABASE_USER"));
 		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
-
-		// // String databaseUrl = dotenv.get("DATABASE_URL", "default_url");
-		// // String databaseUser = dotenv.get("DATABASE_USER", "default_user");
-		// // String databasePassword = dotenv.get("DATABASE_PASSWORD",
-		// // "default_password");
-
-		// System.out.println("DATABASE_URL: ");
-		// System.out.println("DATABASE_USER: ");
-		// System.out.println("DATABASE_PASSWORD: ");
 
 		SpringApplication.run(Main.class, args);
 	}
