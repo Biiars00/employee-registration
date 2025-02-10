2329934a487836eedb985579204edb9d9d32b69b
Beatriz Ribeiro <biiaribeiro60@gmail.com>
2025-02-10 09:18:59 -0300

    ci: deploy config.

--- src/main/java/registration_system/Main.java
+++ src/main/java/registration_system/Main.java
@@ -1,11 +1,19 @@
 package registration_system;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USER", dotenv.get("DATABASE_USER"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
 		SpringApplication.run(Main.class, args);
 	}
 }
